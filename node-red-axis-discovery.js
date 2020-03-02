2b4d64cef4f0a81a4599469275196ac5e782fae0
Fred Juhlin <fred@juhlin.me>
2020-03-02 23:30:53 +0100

    Some testing

--- node-red-axis-discovery.js
+++ node-red-axis-discovery.js
@@ -1,37 +1,36 @@
 const axis = require('axis-discovery');
 
 module.exports = function(RED) {
-  function AxisDiscovery(config) {
-    RED.nodes.createNode(this, config);
-
-    var node = this;
-
-    const discovery = new axis.Discovery();
-    
-    node.on("input", function(msg) {
-      const devices = [];
-
-      discovery.onHello(device => {
-        devices.push(device);
-      })
-
-      setTimeout(
-        () => {
-          discovery.stop();
-
-          msg.payload = devices;
-          node.send(msg);  
-        },
-        5000);
-
-        discovery.start();
-        discovery.search();
-
-      // msg.payload = msg.payload.toLowerCase();
-      // msg.payload = "Some message";
-      // node.send(msg);
-    });
-  }
-  
-  RED.nodes.registerType("Axis Discovery", AxisDiscovery);
+	function AxisDiscovery(config) {
+		RED.nodes.createNode(this, config);
+		var node = this;
+		
+		const discovery = new axis.Discovery();
+		var discoveryMode = false;
+		node.on("input", function(msg) {
+			if(msg.payload === "start" ) {
+				if( discoveryMode === true )
+					return;
+				discovery.onHello(device => {
+					msg.payload = device;
+					node.send(msg);
+				})
+				discovery.start();
+				discovery.search();
+				discoveryMode = true;
+				node.status({fill:"green",shape:"dot",text:"Discovery running"});
+				return;
+			}
+			
+			if(msg.payload === "stop" ) {
+				if( discoveryMode === false )
+					return;
+				discovery.stop();
+				node.status({fill:"red",shape:"dot",text:"Discovery stopped"});
+				discoveryMode = false;
+			}
+		});
+		
+	}
+	RED.nodes.registerType("Axis Discovery", AxisDiscovery);
 };
